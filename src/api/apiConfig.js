1eaf4e042d41538d4bd6a1476b5eeba31c1fb533
Manav Khandurie <manavkhandurie@gmail.com>
2023-10-20 18:02:56 +0530

    Api fixed

--- src/api/apiConfig.js
+++ src/api/apiConfig.js
@@ -1,6 +1,6 @@
 const apiConfig = {
     baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '00e7c549ae2d9b1eda19d3caad272a0b',
+    apiKey: '',//add your api
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
     w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`
 }
--- src/api/firebase.js
+++ src/api/firebase.js
@@ -3,15 +3,15 @@ import { initializeApp } from 'firebase/app'; // Import initializeApp function
 import { getAuth , signInWithPopup ,signInWithEmailAndPassword } from 'firebase/auth'; // Import getAuth function
 import 'firebase/auth';
 import { GoogleAuthProvider, GithubAuthProvider } from 'firebase/auth';
-
+//Add you firebase proj config
 const firebaseConfig = {
-    apiKey: "AIzaSyAoAa8o3Z4ZwTuQAeSt-d0qonVzvanIdWQ",
-    authDomain: "cptproject-750b6.firebaseapp.com",
-    projectId: "cptproject-750b6",
-    storageBucket: "cptproject-750b6.appspot.com",
-    messagingSenderId: "456723411329",
-    appId: "1:456723411329:web:19185011e0f8cfc361b880",
-    measurementId: "G-J1ZC52XKNN"
+    apiKey:"" ,
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   
   const app = initializeApp(firebaseConfig); // Initialize the Firebase app
