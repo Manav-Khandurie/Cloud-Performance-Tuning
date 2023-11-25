b0e4b0e24db0ee82009ff520f2c936b0afc4e5d4
Manav Khandurie <manavkhandurie@gmail.com>
2023-11-25 08:50:31 +0530

    final deploy

--- server/server.js
+++ server/server.js
@@ -2,17 +2,14 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const sql = require('mssql');
 require('dotenv').config();
-const app = express();
-const cors = require("cors");
-app.use(cors()); // Enable CORS for all routes
 
-// Configure bodyParser to parse JSON in the request body
+const app = express();
+const cors = require('cors');
+
+// Enable CORS for all routes
+app.use(cors());
 app.use(bodyParser.json());
-app.use(
-  cors({
-    origin: "*",
-  })
-);
+
 // Configure database connection
 const config = {
   user: process.env.USER,
@@ -28,38 +25,62 @@ const config = {
 const pool = new sql.ConnectionPool(config);
 
 // Define a route to handle JSON requests
-app.post('/insertData', (req, res) => {
+app.post('/insertData', async (req, res) => {
   // Extract name and email from the JSON request
   const { name, email } = req.body;
- 
-  // Connect to the database
-  pool.connect().then((pool) => {
-    // Insert data into the SQL Server table
-    const query = `INSERT INTO ${process.env.TABLE} (Name, Email) VALUES (@name, @email)`;
-    const request = pool.request();
-    request.input('name', sql.VarChar, name);
-    request.input('email', sql.VarChar, email);
-    
-    
 
-    request.query(query).then(() => {
+  try {
+    // Connect to the database
+    const poolConnection = await pool.connect();
+
+    try {
+      // Insert data into the SQL Server table
+      const query = `INSERT INTO ${process.env.TABLE} (Name, Email) VALUES (@name, @email)`;
+      const request = poolConnection.request();
+      request.input('name', sql.VarChar, name);
+      request.input('email', sql.VarChar, email);
+
+      await request.query(query);
+
       console.log('Data inserted successfully');
       res.status(200).send('Data inserted successfully');
-    }).catch((err) => {
-      console.error('Error executing query:', err);
+    } finally {
+      // Release the connection back to the pool
+      poolConnection.release();
+    }
+  } catch (err) {
+    console.error('Error:', err);
+
+    if (err instanceof sql.ConnectionError) {
+      // Handle connection error separately
+      res.status(500).send('Error connecting to the database');
+    } else {
+      // Handle other errors
       res.status(500).send('Internal Server Error');
-    }).finally(() => {
-      // Close the connection pool when done
-      pool.close();
-    });
-  }).catch((err) => {
-    console.error('Error connecting to the database:', err);
-    res.status(500).send('Internal Server Error');
-  });
+    }
+  }
 });
 
 // Start the server
 const PORT = process.env.PORT || 3334;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+});
+
+// Close the connection pool gracefully when the server is stopped
+process.on('SIGTERM', async () => {
+  try {
+    await pool.close();
+    console.log('Connection pool closed');
+  } catch (err) {
+    console.error('Error closing connection pool:', err);
+  } finally {
+    // Ensure the server is stopped
+    server.close();
+  }
+});
+
+// Close the connection pool when the server is stopped
+server.on('close', () => {
+  console.log('Server closed');
 });
